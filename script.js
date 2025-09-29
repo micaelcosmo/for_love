0898d2ace36c3ffa351b0f62f378f320d455ecbc
MicaelCosmo <micael.trabalho@hotmail.com>
2025-09-29 14:53:36 -0300

    .

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
     // 1. VERIFICA SE O ARQUIVO DE CONFIGURAÇÃO FOI CARREGADO
     if (typeof APP_CONFIG !== 'undefined') {
-        const { NOME_DO_PARCEIRO, NOME_DA_PARCEIRA, DATA_INICIO_FORMATADA } = APP_CONFIG;
+        // const { NOME_DO_PARCEIRO, NOME_DA_PARCEIRA, DATA_INICIO_FORMATADA } = APP_CONFIG;
 
         // CORREÇÃO APLICADA AQUI: USAR document.title
-        document.title = `Para a minha ${NOME_DA_PARCEIRA}`;
+        document.title = `Para a minha Ju`;
 
         // Título da Declaração (Data)
-        document.getElementById('titulo-data').textContent = `Onde Tudo Começou (${DATA_INICIO_FORMATADA})`;
+        document.getElementById('titulo-data').textContent = `Onde Tudo Começou (1 de Janeiro de 2025)`;
         
         // Texto da Declaração (Nome e Data)
         const declaracaoP = document.getElementById('texto-declaracao');
         if (declaracaoP) {
             // Injeta a data no texto base do HTML
             declaracaoP.innerHTML = declaracaoP.innerHTML
-                .replace('Desde o dia,', `Desde o dia ${DATA_INICIO_FORMATADA},`)
-                .replace('Você é a minha paz', `${NOME_DA_PARCEIRA}, você é a minha paz`);
+                .replace('Desde o dia,', `Desde o dia 1 de Janeiro de 2025,`)
+                .replace('Você é a minha paz', `Ju, você é a minha paz`);
         }
 
         // Legenda da Foto (Nome dela)
-        document.getElementById('figcaption-ju').textContent = `Aquele sorriso que ilumina meu mundo, minha ${NOME_DA_PARCEIRA}.`;
+        document.getElementById('figcaption-ju').textContent = `Aquele sorriso que ilumina meu mundo, minha Ju.`;
 
         // Assinatura Final (Seu Nome)
-        document.getElementById('assinatura-final').textContent = `Seu ${NOME_DO_PARCEIRO}`;
+        document.getElementById('assinatura-final').textContent = `Seu Kaell`;
     }
     
     // 2. CÓDIGO DO INTERSECTION OBSERVER (FADE-IN/SCROLL)
